6b446bc549afa055a6747629939dbe145e33b436
luozhiyong <luozhiyong131@qq.com>
2023-10-13 15:33:57 +0800

    Update apps.py

--- sdmpWeb/bus/apps.py
+++ sdmpWeb/bus/apps.py
@@ -8,5 +8,5 @@ class BusConfig(AppConfig):
     def ready(self):
             # 启动后台任务的逻辑
             from bus.datastore.orm import manager
-            manager.my_test_fun()
+            # manager.my_test_fun()
             # print("aaaaaaaaaaaaaa")
