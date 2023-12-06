713729709f002bde8c58bc246f181b31dabca0b0
luozhiyong <luozhiyong131@qq.com>
2023-12-06 20:08:24 +0800

    ok

--- sdmpAdmin/application/user_role/services.py
+++ sdmpAdmin/application/user_role/services.py
@@ -3,8 +3,8 @@ from application.user_role.models import UserRole
 
 # 根据用户ID查询角色列表
 def getUserRoleList(userId):
-    sql = 'SELECT r.* FROM django_role AS r '
-    sql += 'INNER JOIN django_user_role AS ur ON r.id=ur.role_id '
+    sql = 'SELECT r.* FROM sdmp_role AS r '
+    sql += 'INNER JOIN sdmp_user_role AS ur ON r.id=ur.role_id '
     sql += 'WHERE ur.user_id=%s AND r.is_delete=0'
     list = UserRole.objects.raw(sql, [userId])
     # 实例化角色列表
--- sdmpAdmin/config/env.py
+++ sdmpAdmin/config/env.py
@@ -16,7 +16,7 @@ DEBUG = False
 # 使用mysql时，改为此配置
 DATABASE_ENGINE = "django.db.backends.mysql"
 # 数据库库名
-DATABASE_NAME = 'djangoadmin'  # mysql 时使用
+DATABASE_NAME = 'sdmp_admin'  # mysql 时使用
 # 数据库地址 改为自己数据库地址
 DATABASE_HOST = "127.0.0.1"
 # 数据库端口
@@ -26,12 +26,12 @@ DATABASE_USER = "root"
 # 数据库密码
 DATABASE_PASSWORD = "123456"
 # 数据表前缀
-TABLE_PREFIX = "django_"
+TABLE_PREFIX = "sdmp_"
 
 # 全局变量
 # 图片地址
 IMAGE_URL = "http://images.django.elevue"
 # 附件存储目录
-ATTACHMENT_PATH = "D:/DjangoAdmin/DjangoAdmin_Django_EleVue/public/uploads"
+ATTACHMENT_PATH = "/home/lzy/work/SDMP/sdmpAdmin/public/uploads"
 IMAGE_PATH = '{}/images'.format(ATTACHMENT_PATH)
 TEMP_PATH = '{}/temp'.format(ATTACHMENT_PATH)
